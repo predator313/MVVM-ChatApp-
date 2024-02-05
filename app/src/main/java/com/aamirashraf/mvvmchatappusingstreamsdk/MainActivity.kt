588a95c08d288c96e93a30d9d5dd42201c076356
Android Dev <aamirnasa97@gmail.com>
2024-02-05 23:20:18 +0530

    error in stream side

--- app/src/main/java/com/aamirashraf/mvvmchatappusingstreamsdk/MainActivity.kt
+++ app/src/main/java/com/aamirashraf/mvvmchatappusingstreamsdk/MainActivity.kt
@@ -10,6 +10,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //there is some error from stream side
+        //so there is issue in the project
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
